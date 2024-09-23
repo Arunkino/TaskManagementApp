4ca313c3a6791b3396b3ff9c49b5af2a0ea7bdaf
Arunkino <150350015+Arunkino@users.noreply.github.com>
2024-09-23 23:09:25 +0530

    Update axiosConfig.js
    url changed to http://api.taskmanagement.site/api

--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api/';
-
+// const API_URL = 'http://localhost:8000/api/';
+const API_URL = 'http://api.taskmanagement.site/api/';
 const axiosInstance = axios.create({
   baseURL: API_URL,
   timeout: 5000,
